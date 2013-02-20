d60bb868e8be693530d52d4b6d8c4e000d6c2665
manfred quasten <manfred.quasten@arcor.de>
2013-02-20 18:13:45 +0100

    xxx

--- mapping.util/src/main/java/de/mq/mapping/util/proxy/support/SpringBeanResolverImpl.java
+++ mapping.util/src/main/java/de/mq/mapping/util/proxy/support/SpringBeanResolverImpl.java
@@ -2,13 +2,9 @@ package de.mq.mapping.util.proxy.support;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.ApplicationContext;
-import org.springframework.context.annotation.Profile;
-import org.springframework.stereotype.Component;
 
 import de.mq.mapping.util.proxy.BeanResolver;
 
-@Component()
-@Profile({"CGLib-Proxy", "Dynamic-Proxy"})
 public class SpringBeanResolverImpl implements BeanResolver{
 	
 	private final ApplicationContext applicationContext;
