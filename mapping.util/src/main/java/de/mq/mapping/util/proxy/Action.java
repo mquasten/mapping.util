feb42cd7b16e84bec5e0db31f2b0579b388f6eef
manfred quasten <manfred.quasten@arcor.de>
2013-02-20 16:32:56 +0100

    test

--- mapping.util/src/main/java/de/mq/mapping/util/proxy/Action.java
+++ mapping.util/src/main/java/de/mq/mapping/util/proxy/Action.java
@@ -3,5 +3,4 @@ package de.mq.mapping.util.proxy;
 
 public interface  Action  {
 	Object execute(final Class<?> result, final String bundle, final ModelRepository modelRepository) throws Exception;
-    
 }
