4af0f5690260a2483f60873942234bac3b207a87
Manfred Quasten <manfred.quasten@arcor.de>
2013-02-20 20:27:55 +0100

    add java doc to key interface

--- mapping.util/src/main/java/de/mq/mapping/util/proxy/support/Key.java
+++ mapping.util/src/main/java/de/mq/mapping/util/proxy/support/Key.java
@@ -1,12 +1,30 @@
 package de.mq.mapping.util.proxy.support;
 
+/**
+ * The Key in a map for the entries in the repository.
+ * This key is used, to differ between domainClasses, attributes, errors  
+ * @author MQuasten
+ *
+ */
 interface Key {
 
+	/**
+	 * Is the given lass is a child from an other.
+	 * @param clazz the class that should be checked
+	 * @return true if it has a parent, it is an child else false
+	 */
 	boolean hasParent(final Class<?> clazz);
 	
+	/**
+	 * The key isn't written, read from a domainClass. It is simply chached in a map.
+	 * @return true if non domainObject is used
+	 */
 	boolean isMapKey(); 
-	
-	
+
+	/**
+	 * The name of the key
+	 * @return the name from key
+	 */
 	String name();
 
 }
