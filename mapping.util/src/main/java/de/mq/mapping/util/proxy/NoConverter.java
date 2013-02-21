a7cf5268e993827bede0c68a2cb9d75f031d60c2
Manfred Quasten <manfred.quasten@arcor.de>
2013-02-21 20:18:24 +0100

    NOConverter is used too

--- mapping.util/src/main/java/de/mq/mapping/util/proxy/NoConverter.java
+++ mapping.util/src/main/java/de/mq/mapping/util/proxy/NoConverter.java
@@ -1,7 +1,10 @@
 package de.mq.mapping.util.proxy;
 
+import org.springframework.context.annotation.Profile;
 import org.springframework.core.convert.converter.Converter;
-
+import org.springframework.stereotype.Component;
+@Component
+@Profile({"CGLib-Proxy", "Dynamic-Proxy"})
 public class NoConverter implements Converter<Object,Object> {
 
 	@Override
