852ca668fe29a323e8016e5745be752b08754108
Manfred Quasten <manfred.quasten@arcor.de>
2014-04-11 20:19:15 +0200

    BasicImplementation for a NullObjectResolver

--- mapping.util/src/test/java/de/mq/mapping/util/proxy/support/NullObjectResolverTest.java
+++ mapping.util/src/test/java/de/mq/mapping/util/proxy/support/NullObjectResolverTest.java
@@ -11,16 +11,21 @@ import de.mq.mapping.util.proxy.NullObjectResolver;
 
 public class NullObjectResolverTest {
 	
+	private static final String ARTIST_NAME = "Kylie Ann Minoge";
+
+
 	@Test
 	public final void add() {
 		final Artist artist = Mockito.mock(Artist.class);
-		Mockito.when(artist.name()).thenReturn("Kylie Ann Minoge");
+		Mockito.when(artist.name()).thenReturn(ARTIST_NAME);
 		final BasicNullObjectResolverImpl  resolver = new BasicNullObjectResolverImpl(); 
 		resolver.put(Artist.class, artist);
 		resolver.put(artist.getClass(), artist);
 		
 		Assert.assertEquals(artist, resolver.forType(Artist.class));
 		Assert.assertEquals(artist, resolver.forType(artist.getClass()));
+		
+		Assert.assertEquals(ARTIST_NAME, resolver.forType(Artist.class).name());
 	}
 	
 	
