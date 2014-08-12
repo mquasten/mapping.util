119066c01ef2e415edbc8ac38cafd14510965c8d
Manfred Quasten <manfred.quasten@arcor.de>
2014-08-12 19:52:13 +0200

    CouchTemplate as springbean

--- mapping.util/src/main/java/de/mq/mapping/util/json/support/SimpleMapBasedResponseClassFactoryImpl.java
+++ mapping.util/src/main/java/de/mq/mapping/util/json/support/SimpleMapBasedResponseClassFactoryImpl.java
@@ -14,6 +14,7 @@ import net.sf.cglib.proxy.MethodInterceptor;
 import net.sf.cglib.proxy.MethodProxy;
 import net.sf.cglib.proxy.NoOp;
 
+import org.springframework.stereotype.Component;
 import org.springframework.util.Assert;
 import org.springframework.util.SerializationUtils;
 
@@ -21,7 +22,7 @@ import de.mq.mapping.util.json.MapBasedResponseClassFactory;
 import de.mq.mapping.util.json.MapBasedResultBuilder;
 
 
-
+@Component
 public class SimpleMapBasedResponseClassFactoryImpl implements MapBasedResponseClassFactory {
 	
 	
